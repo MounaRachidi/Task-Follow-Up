0de2bc7066797b8cb8c3034f6d207cc0b1226f24
MounaRachidi <106609337+MounaRachidi@users.noreply.github.com>
2022-05-31 16:28:39 +0100

    [NBUI] Commit local changes

---
+++ ios/SplashScreenPresenter.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+#import <MendixNative/SplashScreenPresenterProtocol.h>
+
+@interface SplashScreenPresenter : NSObject<SplashScreenPresenterProtocol>
+@end
---
+++ ios/nativeTemplate-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "AppDelegate.h"
+#import <MendixNative/MendixNative_Framework.h>
+#import "SplashScreenPresenter.h"
+
